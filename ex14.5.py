575e0c569ba8bbb3cea4ca37c4fdfcfa3a51203a
Cloris <294586916@qq.com>
2018-07-28 15:06:27 +0800

    submit homework

---
+++ ex14.5.py
@@ -0,0 +1,15 @@
+a=(1,2,3)
+b={1,2,3}
+for i in a:
+    print i
+    print 1+3
+print "%%%%%%%%%%%%%%%%"
+for i in b:
+    print i
+print "%%%%%%%%%%%%%%%%"
+for i in range(3):
+    print a[i]
+print "%%%%%%%%%%%%%%%%"
+c={1:'a',2:'b',3:'c'}
+for i in b:
+    print c[i]
