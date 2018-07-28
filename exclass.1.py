4935f7e6b0c6b8fc11b2c769f6cab0297141f5ea
Cloris <294586916@qq.com>
2018-07-28 19:35:51 +0800

    class example

---
+++ exclass.1.py
@@ -0,0 +1,5 @@
+import exclass as mc
+apple=mc.fruit()
+print apple.edible
+print apple.cook
+apple.peel()
---
+++ exclass.py
@@ -0,0 +1,6 @@
+class fruit(object):
+    def __init__(self):
+        self.edible=True
+        self.cook=False
+    def peel(self):
+        print"Peel me."
