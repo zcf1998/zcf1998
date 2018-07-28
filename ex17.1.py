ec7920be4634eed9dc235e0c90102d5ac3f16800
Cloris <294586916@qq.com>
2018-07-28 19:31:44 +0800

    module

---
+++ ex17.1.py
@@ -0,0 +1,5 @@
+import ex17 as mf
+C_earth=mf.Cir(6378.0)
+print "The circumference of earth is %f."%(C_earth)
+C_mars=mf.Cir(3396.0)
+print "The circumference of mars is %f."%(C_mars)
---
+++ ex17.py
@@ -0,0 +1,3 @@
+import numpy as np
+def Cir(r):
+    return 2*np.pi*r
